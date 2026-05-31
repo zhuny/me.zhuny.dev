b56b2f6a866e89e44f6e1f8af8cea09d70a77181
DESKTOP-3DMF6CO\zhuny <zhuny936772@gmail.com>
2026-05-31 22:57:45 +0900

    first my introduction

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'zhuny';
+export const SITE_DESCRIPTION = '생각을 구조로, 가치를 경험으로';
